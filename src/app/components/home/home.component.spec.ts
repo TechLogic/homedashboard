42b66860f043c270caee4fc153b2b413066bb25a
Techlogic <nils.heyer@gmx.de>
2017-07-23 01:08:12 +0200

    Add material components to test to fixed it

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-
+import { MaterialModule } from "@angular/material";
 import { HomeComponent } from "./home.component";
 
 describe("HomeComponent", () => {
@@ -9,7 +9,8 @@ describe("HomeComponent", () => {
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
-        declarations: [HomeComponent]
+        declarations: [HomeComponent],
+        imports: [MaterialModule]
       }).compileComponents();
     })
   );
