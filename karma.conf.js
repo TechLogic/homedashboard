7a9adaa566e3586e13e5e16986112f452dfdd3ef
Techlogic <nils.heyer@gmx.de>
2017-07-22 20:26:38 +0200

    Add junit report to karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,44 +1,54 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular/cli"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("@angular/cli/plugins/karma"),
+      require("karma-junit-reporter")
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    files: [
-      { pattern: './src/test.ts', watched: false }
-    ],
+    files: [{ pattern: "./src/test.ts", watched: false }],
     preprocessors: {
-      './src/test.ts': ['@angular/cli']
+      "./src/test.ts": ["@angular/cli"]
     },
     mime: {
-      'text/x-typescript': ['ts','tsx']
+      "text/x-typescript": ["ts", "tsx"]
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ["html", "lcovonly"],
       fixWebpackSourcePaths: true
     },
     angularCli: {
-      environment: 'dev'
+      environment: "dev"
     },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul']
-              : ['progress', 'kjhtml'],
+    reporters:
+      config.angularCli && config.angularCli.codeCoverage
+        ? ["progress", "coverage-istanbul"]
+        : ["progress", "junit"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ["Chrome"],
+    singleRun: true,
+    junitReporter: {
+      outputDir: "dist/test", // results will be saved as $outputDir/$browserName.xml
+      outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
+      suite: "", // suite will become the package name attribute in xml testsuite element
+      useBrowserName: false, // add browser name to report and classes names
+      nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+      classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+      properties: {}, // key value pair of properties to add to the <properties> section of the report
+      xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+    }
   });
 };
